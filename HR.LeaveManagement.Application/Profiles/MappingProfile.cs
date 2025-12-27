d39e6202849fa38216311d5167efc910bcb71593
emmanuel-tech58 <esngwala@gmail.com>
2025-12-27 17:40:54 +0200

    Add DTOs and AutoMapper profile for leave management
    Introduced DTO classes for leave types, allocations, and requests, including a shared BaseDTO. Added MappingProfile to configure AutoMapper mappings between domain models and DTOs. Organized DTOs under appropriate namespaces and set access modifiers as needed.

---
+++ HR.LeaveManagement.Application/Profiles/MappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.DTOs;
+using HR.LeaveManagement.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.Profiles
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+             CreateMap<LeaveType, LeaveTypeDTO>().ReverseMap();
+            CreateMap<LeaveAllocation, LeaveAllocationDTO>().ReverseMap();
+            CreateMap<LeaveRequest, LeaveRequestDTO>().ReverseMap();
+        }
+    }
+}
